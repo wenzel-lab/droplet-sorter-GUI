a81d1e0c9cbfc821746c1a3d3be4d2f5223c01ac
the-alquemist <jacosta1@hotmail.cl>
2025-01-27 14:06:38 -0300

    fpga_acquisition

--- ui_layout.py
+++ ui_layout.py
@@ -989,11 +989,13 @@ class UI:
                 "title" : "Noise (width)",
                 "value" : "123",
                 "disabled" : False,
+                "callback" : self._noise_width_changed,
             },
             {
                 "title" : "Noise (auc)",
                 "value" : "123",
                 "disabled" : False,
+                "callback" : self._noise_auc_changed,
             },
         ]
         
@@ -1006,6 +1008,7 @@ class UI:
                 width = 100,
                 margin = [120,20,20,100],
             )
+            custom_noise_box.on_change("value", noise_info["callback"])
             self.noise_boxes.append(custom_noise_box)
 
         return self.noise_boxes
@@ -1685,7 +1688,7 @@ class UI:
         else:
             self.scatter_toggle.label = "Start"
             self.scatter_toggle.button_type = "primary"
-            self.fpga_data.stop_acquisiton() #(dg)
+            self.fpga_data.stop_acquisition() #(dg)
             self.no_update = 1 #LibreHub
 
     def _toggle_sipm_signal(self, attr, old, new): #LibreHub
@@ -1781,6 +1784,14 @@ class UI:
     def _thresh_changed(self, attr, old, new):
         self.fpga_data.set_thresh(new) #(dg)
         self.thresh_line.location = self.sliders[6].value
+
+    def _noise_width_changed(self, attr, old, new):
+        #self.fpga_data.set_fpga_register_value(self, var_name, value, addr=0) #(dg)
+        pass
+
+    def _noise_auc_changed(self, attr, old, new):
+        #self.fpga_data.set_fpga_register_value(self, var_name, value, addr=0) #(dg)
+        pass
 
     # Add def for each threshold as needed, in the case of the simulation, seem to be combined between channels.
 
